f9436d5e19230bd1e6ad598ad51f5fd7192fadb8
Diogo Munhoz Fraga <digmunhoz@gmail.com>
2021-04-01 22:50:18 -0300

    ;)

--- modules/kubernetes/deployment/main.tf
+++ modules/kubernetes/deployment/main.tf
@@ -11,13 +11,13 @@ resource "kubernetes_deployment" "deployment" {
     strategy {
       type = "RollingUpdate"
       rolling_update {
-        max_surge = "50%"
+        max_surge       = "50%"
         max_unavailable = 0
       }
     }
 
-    replicas                = var.replicas
-    revision_history_limit  = 3
+    replicas               = var.replicas
+    revision_history_limit = 3
     selector {
       match_labels = {
         app = var.name
@@ -33,7 +33,7 @@ resource "kubernetes_deployment" "deployment" {
 
       spec {
         affinity {
-          pod_affinity {
+          pod_anti_affinity {
             required_during_scheduling_ignored_during_execution {
               label_selector {
                 match_expressions {
@@ -47,26 +47,26 @@ resource "kubernetes_deployment" "deployment" {
           }
         }
         container {
-          image = "${var.registry}/${ var.image }"
+          image             = "${var.registry}/${var.image}"
           image_pull_policy = var.image_pull_policy
-          name  = var.name
+          name              = var.name
 
 
           dynamic "volume_mount" {
             for_each = var.mount_path != "" ? [var.mount_path] : []
             content {
               mount_path = var.mount_path
-              name = "source-code"
-              read_only = "false"
+              name       = "source-code"
+              read_only  = "false"
             }
           }
 
           dynamic "env" {
-             for_each = var.env
-             content {
-               name = env.key
-               value = env.value
-             }
+            for_each = var.env
+            content {
+              name  = env.key
+              value = env.value
+            }
           }
 
           resources {
@@ -82,7 +82,21 @@ resource "kubernetes_deployment" "deployment" {
 
           liveness_probe {
             tcp_socket {
-              port    = var.ports[0]
+              port = var.ports[0]
+            }
+            # http_get {
+            #   path    = "/"
+            #   port    = var.ports[0]
+            #   scheme  = var.protocol
+            # }
+            initial_delay_seconds = var.liveness_probe["initial_delay_seconds"]
+            period_seconds        = var.liveness_probe["period_seconds"]
+            success_threshold     = var.liveness_probe["success_threshold"]
+            timeout_seconds       = var.liveness_probe["timeout_seconds"]
+          }
+          readiness_probe {
+            tcp_socket {
+              port = var.ports[0]
             }
             # http_get {
             #   path    = "/"
@@ -95,9 +109,15 @@ resource "kubernetes_deployment" "deployment" {
             timeout_seconds       = var.liveness_probe["timeout_seconds"]
           }
         }
-        dns_policy                        = var.dns_policy
-        restart_policy                    = "Always"
-        termination_grace_period_seconds  = 30
+        dns_policy = var.dns_policy
+        dns_config {
+          option {
+            name = "ndots"
+            value = 1
+          }
+        }
+        restart_policy                   = "Always"
+        termination_grace_period_seconds = 30
         dynamic "volume" {
           for_each = var.root_path != "" ? [var.root_path] : []
           content {
