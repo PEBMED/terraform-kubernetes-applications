dbd7862a01c99dd6334aa8a02c68c4427628b6f4
Diogo Munhoz Fraga <digmunhoz@gmail.com>
2021-04-08 15:18:38 -0300

    Changing service for develop environment

--- modules/kubernetes/service/main.tf
+++ modules/kubernetes/service/main.tf
@@ -21,7 +21,8 @@ locals {
   }"
 }
 
-resource "kubernetes_service" "service" {
+resource "kubernetes_service" "develop" {
+  count = var.environment == "develop" ? 1 : 0
   metadata {
     name = var.name
     annotations = {
@@ -50,6 +51,44 @@ resource "kubernetes_service" "service" {
         protocol = var.protocol
         name = "port-${port.value}"
         node_port = var.node_port != "" && port.key == 0 ? var.node_port : 0
+      }
+    }
+
+    type       = local.service_type
+    cluster_ip = var.ip_address
+  }
+}
+
+
+resource "kubernetes_service" "homolog_and_production" {
+  count = var.environment != "develop" ? 1 : 0
+  metadata {
+    name = var.name
+    annotations = {
+      "external-dns.alpha.kubernetes.io/aws-weight": "100"
+      "external-dns.alpha.kubernetes.io/hostname": "${var.name}.${var.root_domain}"
+      "service.beta.kubernetes.io/aws-load-balancer-ssl-cert": var.aws_cert_arn
+      "service.beta.kubernetes.io/aws-load-balancer-connection-idle-timeout": "600"
+      "service.beta.kubernetes.io/aws-load-balancer-connection-draining-enabled": "false"
+      "service.beta.kubernetes.io/aws-load-balancer-backend-protocol": local.loadbalancer_protocol
+      "service.beta.kubernetes.io/aws-load-balancer-ssl-ports": local.loadbalancer_ssl_port
+      "external-dns.alpha.kubernetes.io/set-identifier": var.environment
+      "service.beta.kubernetes.io/aws-load-balancer-internal": local.visibility_annotation
+    }
+  }
+  spec {
+    selector = {
+      app = var.name
+    }
+    session_affinity = "None"
+
+    dynamic "port" {
+      for_each = var.ports
+      content {
+        port = port.value
+        target_port = port.value
+        protocol = var.protocol
+        name = "port-${port.value}"
       }
     }
 
