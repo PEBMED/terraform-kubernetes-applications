cf7140689e9c82198d6e3e171fa591f7a2f0c2d1
Diogo Munhoz Fraga <digmunhoz@gmail.com>
2021-04-15 21:11:19 -0300

    New deployment for production environmen

--- modules/kubernetes/deployment/main.tf
+++ modules/kubernetes/deployment/main.tf
@@ -6,8 +6,115 @@ locals {
   }"
 }
 
-resource "kubernetes_deployment" "deployment" {
+resource "kubernetes_deployment" "deployment_develop_homolog" {
+  count = var.environment != "production" ? 1 : 0
+  metadata {
+    name = var.name
+    labels = {
+      app = var.name
+    }
+  }
 
+  spec {
+    strategy {
+      type = "RollingUpdate"
+      rolling_update {
+        max_surge       = "50%"
+        max_unavailable = local.max_unavailable
+      }
+    }
+
+    replicas               = var.replicas
+    revision_history_limit = 3
+    selector {
+      match_labels = {
+        app = var.name
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = var.name
+        }
+      }
+
+      spec {
+        container {
+          image             = "${var.registry}/${var.image}"
+          image_pull_policy = var.image_pull_policy
+          name              = var.name
+
+          dynamic "volume_mount" {
+            for_each = var.mount_path != "" ? [var.mount_path] : []
+            content {
+              mount_path = var.mount_path
+              name       = "source-code"
+              read_only  = "false"
+            }
+          }
+
+          dynamic "env" {
+            for_each = var.env
+            content {
+              name  = env.key
+              value = env.value
+            }
+          }
+
+          resources {
+            requests = {
+              cpu    = var.requests["cpu"]
+              memory = var.requests["memory"]
+            }
+          }
+
+          liveness_probe {
+            tcp_socket {
+              port = var.ports[0]
+            }
+            initial_delay_seconds = var.probe["initial_delay_seconds"]
+            period_seconds        = var.probe["period_seconds"]
+            success_threshold     = var.probe["success_threshold"]
+            timeout_seconds       = var.probe["timeout_seconds"]
+          }
+          readiness_probe {
+            tcp_socket {
+              port = var.ports[0]
+            }
+            initial_delay_seconds = var.probe["initial_delay_seconds"]
+            period_seconds        = var.probe["period_seconds"]
+            success_threshold     = var.probe["success_threshold"]
+            timeout_seconds       = var.probe["timeout_seconds"]
+          }
+        }
+        dns_policy = var.dns_policy
+        dns_config {
+          option {
+            name = "ndots"
+            value = 1
+          }
+        }
+        restart_policy                   = "Always"
+        termination_grace_period_seconds = 30
+        dynamic "volume" {
+          for_each = var.root_path != "" ? [var.root_path] : []
+          content {
+            host_path { path = var.root_path }
+            name = "source-code"
+          }
+        }
+      }
+    }
+  }
+  lifecycle {
+    ignore_changes = [spec[0].replicas]
+  }
+}
+
+
+resource "kubernetes_deployment" "deployment_production" {
+  count = var.environment != "production" ? 0 : 1
   metadata {
     name = var.name
     labels = {
@@ -59,13 +166,12 @@ resource "kubernetes_deployment" "deployment" {
           image_pull_policy = var.image_pull_policy
           name              = var.name
 
-
-          dynamic "volume_mount" {
-            for_each = var.mount_path != "" ? [var.mount_path] : []
-            content {
-              mount_path = var.mount_path
-              name       = "source-code"
-              read_only  = "false"
+          env_from {
+            secret_ref {
+              name = "shared"
+            }
+            secret_ref {
+              name = var.name
             }
           }
 
@@ -83,34 +189,28 @@ resource "kubernetes_deployment" "deployment" {
               memory = var.requests["memory"]
             }
             limits = {
-              cpu    = var.environment == "production" ? var.requests["cpu"] : "1000m"
-              memory = var.environment == "production" ? var.requests["memory"] : "500Mi"
+              cpu    = var.requests["cpu"]
+              memory = var.requests["memory"]
             }
           }
 
           liveness_probe {
-            tcp_socket {
-              port = var.ports[0]
+            http_get {
+              path    = "/healthcheck"
+              port    = var.ports[0]
+              scheme  = var.protocol
             }
-            # http_get {
-            #   path    = "/healthcheck"
-            #   port    = var.ports[0]
-            #   scheme  = var.protocol
-            # }
             initial_delay_seconds = var.probe["initial_delay_seconds"]
             period_seconds        = var.probe["period_seconds"]
             success_threshold     = var.probe["success_threshold"]
             timeout_seconds       = var.probe["timeout_seconds"]
           }
           readiness_probe {
-            tcp_socket {
-              port = var.ports[0]
+            http_get {
+              path    = "/healthcheck"
+              port    = var.ports[0]
+              scheme  = var.protocol
             }
-            # http_get {
-            #   path    = "/healthcheck"
-            #   port    = var.ports[0]
-            #   scheme  = var.protocol
-            # }
             initial_delay_seconds = var.probe["initial_delay_seconds"]
             period_seconds        = var.probe["period_seconds"]
             success_threshold     = var.probe["success_threshold"]
@@ -126,13 +226,6 @@ resource "kubernetes_deployment" "deployment" {
         }
         restart_policy                   = "Always"
         termination_grace_period_seconds = 30
-        dynamic "volume" {
-          for_each = var.root_path != "" ? [var.root_path] : []
-          content {
-            host_path { path = var.root_path }
-            name = "source-code"
-          }
-        }
       }
     }
   }
