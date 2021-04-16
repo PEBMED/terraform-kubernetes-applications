b7016503b00433f09390030856cce8d9d59ca719
Diogo Munhoz Fraga <digmunhoz@gmail.com>
2021-04-15 21:45:06 -0300

    Split homolog and production service

--- modules/kubernetes/service/main.tf
+++ modules/kubernetes/service/main.tf
@@ -64,9 +64,8 @@ resource "kubernetes_service" "develop" {
   }
 }
 
-
-resource "kubernetes_service" "homolog_and_production" {
-  count = var.environment != "develop" ? 1 : 0
+resource "kubernetes_service" "homolog" {
+  count = var.environment == "homolog" ? 1 : 0
   metadata {
     name = var.name
     annotations = {
@@ -99,5 +98,41 @@ resource "kubernetes_service" "homolog_and_production" {
 
     type       = local.service_type_production
     cluster_ip = var.ip_address
+  }
+}
+
+resource "kubernetes_service" "production" {
+  count = var.environment == "production" ? 1 : 0
+  metadata {
+    name = var.name
+    annotations = {
+      "external-dns.alpha.kubernetes.io/aws-weight": "100"
+      "external-dns.alpha.kubernetes.io/hostname": "${var.name}.${var.root_domain}"
+      "service.beta.kubernetes.io/aws-load-balancer-ssl-cert": var.aws_cert_arn
+      "service.beta.kubernetes.io/aws-load-balancer-connection-idle-timeout": "600"
+      "service.beta.kubernetes.io/aws-load-balancer-connection-draining-enabled": "false"
+      "service.beta.kubernetes.io/aws-load-balancer-backend-protocol": local.loadbalancer_protocol
+      "service.beta.kubernetes.io/aws-load-balancer-ssl-ports": local.loadbalancer_ssl_port
+      "external-dns.alpha.kubernetes.io/set-identifier": var.environment
+      "service.beta.kubernetes.io/aws-load-balancer-internal": local.visibility_annotation
+    }
+  }
+  spec {
+    selector = {
+      app = var.name
+    }
+    session_affinity = "None"
+
+    dynamic "port" {
+      for_each = var.ports
+      content {
+        port = port.value
+        target_port = port.value
+        protocol = var.protocol
+        name = "port-${port.value}"
+      }
+    }
+
+    type       =  "LoadBalancer"
   }
 }
